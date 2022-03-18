e9ac882f3833c14b09754fdcf4f0b2e8dbc5c935
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-18 11:36:20 -0600

    added rollbar functionality

--- server.js
+++ server.js
@@ -11,11 +11,17 @@ app.use(express.json())
 app.use(cors())
 
 //rollbar stuff
+var rollbar = new Rollbar({
+    accessToken: 'be0c3edd65044088b78755398300911a',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  })
 
 //generic rollbar message
-// rollbar.log('Hello world!')
+rollbar.log('Hello world!')
 
 app.get("/", function(req, res) {
+    rollbar.info("HTML has been linked")
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
@@ -25,8 +31,10 @@ app.get("/js", (req, res) => {
 
 app.get('/api/robots', (req, res) => {
     try {
+        rollbar.info("someone can see all the bots")
         res.status(200).send(bots)
     } catch (error) {
+        rollbar.error("there is an error getting bots")
         console.log('ERROR GETTING BOTS', error)
         res.sendStatus(400)
     }
@@ -34,11 +42,13 @@ app.get('/api/robots', (req, res) => {
 
 app.get('/api/robots/five', (req, res) => {
     try {
+        rollbar.info("someone drew 5 bots to choose from")
         let shuffled = shuffleArray(bots)
         let choices = shuffled.slice(0, 5)
         let compDuo = shuffled.slice(6, 8)
         res.status(200).send({choices, compDuo})
     } catch (error) {
+        rollbar.error("someone didn't recieve 5 bots")
         console.log('ERROR GETTING FIVE BOTS', error)
         res.sendStatus(400)
     }
@@ -46,6 +56,7 @@ app.get('/api/robots/five', (req, res) => {
 
 app.post('/api/duel', (req, res) => {
     try {
+        rollbar.info("someone wants to duel")
         // getting the duos from the front end
         let {compDuo, playerDuo} = req.body
 
@@ -63,10 +74,12 @@ app.post('/api/duel', (req, res) => {
 
         // comparing the total health to determine a winner
         if (compHealthAfterAttack > playerHealthAfterAttack) {
+            rollbar.warning("someone lost a duel")
             playerRecord.losses++
             res.status(200).send('You lost!')
         } else {
-            playerRecord.losses++
+            rollbar.info("someone won a duel")
+            playerRecord.wins++
             res.status(200).send('You won!')
         }
     } catch (error) {
